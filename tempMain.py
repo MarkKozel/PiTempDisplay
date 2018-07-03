24418075f0800f22d81068fa65207eece4dfc42d
markkozel <Development@MarkKozel.net>
2018-07-03 15:36:22 -0700

    Added write-to-file dump

--- tempMain.py
+++ tempMain.py
@@ -22,14 +22,19 @@
 import sys
 import time
 import datetime
-import Adafruit_DHT
 
+import signal
+
+import shutil #For file rename
+
+import Adafruit_DHT
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
 
 
 # Parse command line parameters.
@@ -76,40 +81,72 @@ bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-
 # Load default font.
 font = ImageFont.load_default()
 
-while 1:
+csvFile = open("temp.dat", 'w')
+csvFile.write("Date\tTime\tHumidity\tTemperature\n")
+
+#Setup exit on SIG (from Pi shutdown) or CTRL
+keepRunning = True
+
+def exit_gracefully(signal, frame):
+  print("Exiting due to " + str(signal))
+  keepRunning = False
+  csvFile.close()
+  #Rename file for archive
+  dt = datetime.datetime.now()
+  newFileName = "temp_" + str(dt.strftime('%Y-%m-%d')) + "_" +str(dt.strftime('%H:%M:%S.%f')) + ".dat"
+  print(newFileName)
+  shutil.move("temp.dat", newFileName)
+  #Buh-bye
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_gracefully)
+signal.signal(signal.SIGTERM, exit_gracefully)
+
+#Write to file evey wtiteInterval'th time thru the forever loop
+writeFlag = 0 #Keeps count of forever loop, ++'d each time thru
+writeInterval = 10 #Compare current writeFlag to this to see if it is the writeInteraval'th time
+
+while keepRunning:
   dt = datetime.datetime.now()
 
-  # Try to grab a sensor reading.  Use the read_retry method which will retry up
-  # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+  # Read from sensor
   humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-  # Un-comment the line below to convert the temperature to Fahrenheit.
-  temperature = temperature * 9/5.0 + 32
+  # Convert to F if we got good data
+  if temperature is not None:
+    temperature = temperature * 9/5.0 + 32
 
+  newData = ""
   # Note that sometimes you won't get a reading and the results will be null (because Linux can't
   # guarantee the timing of calls to read the sensor).
   # If this happens try again!
   if humidity is not None and temperature is not None:
-    print('{0},\t{1},\tTemp={2:0.1f},\tHumidity={3:0.1f}%'.format(dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S'), temperature, humidity))
+    newData = '{0}\t{1}\t{2:0.1f}\t{3:0.1f}%'.format(dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S'), temperature, humidity)
   else:
-    print('Missed reading')
+     newData = '{0}\t{1}\tNo Data\tNo Data'.format(dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S'))
+  
+  #Print and Save data
+  print(newData)
+  if writeFlag % writeInterval is 0:
+    csvFile.write(newData + "\n")
+    writeFlag = 0 #Reset write flag
+  else:
+    writeFlag += 1 #Bump write flag for next cycle
 
+  #Update display
   draw.rectangle((0,0,width,height), outline=0, fill=0)
 
   draw.text((x, top),       "Date: " + dt.strftime('%Y-%m-%d'),  font=font, fill=255)
   draw.text((x, top+8),     "Time: " + dt.strftime('%H:%M:%S'),  font=font, fill=255)
   draw.text((x, top+16),    "Temp: " + str(temperature),  font=font, fill=255)
   draw.text((x, top+25),    "Rh  : " + str(humidity),  font=font, fill=255)
-  # draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-  # draw.text((x, top+25),    str(Disk),  font=font, fill=255)
 
   # Display image.
-  # disp.image(image)
   disp.image(image.rotate(180))
   disp.display()
 
-  time.sleep(2)
+  if keepRunning is True:
+    time.sleep(1)
